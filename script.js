b07f2b2c2c0cee11d6a25745754e0b4e624094f0
Johnathan <jonathanumana86@gmail.com>
2021-12-13 18:17:47 -0500

    Will be using a table to make it ornagized & Easier to undertsand

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // const overlay = document.querySelector('.overlay')
 // const btnCloseModal = document.querySelector('.close-modal')
 // const btnsSHowModal = document.querySelectorAll('.show-modal')
-document.querySelectorAll('.checked').addEventListener('click', function (){
+document.getElementById("check").addEventListener('click', function (){
   console.log('====================================');
   console.log(check);
   console.log('====================================');
