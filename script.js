7e3d7b3d3dd01cfef71cbb36ff2039ca59691721
Johnathan238 <jonathanumana86@gmail.com>
2021-12-13 00:50:01 -0500

    Still working on the button but i will get it done, My head is killing me

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const modal = document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector('.close-modal')
-const btnsSHowModal = document.querySelectorAll('.show-modal')
-const checkBox = document.getElementById("1")
-
-checkBox.addEventListener('click', function (){
-  modal.classList.remove('hidden')
-  overlay.classList.remove('hidden')
+// const modal = document.querySelector('.modal')
+// const overlay = document.querySelector('.overlay')
+// const btnCloseModal = document.querySelector('.close-modal')
+// const btnsSHowModal = document.querySelectorAll('.show-modal')
+document.querySelectorAll('.checked').addEventListener('click', function (){
+  console.log('====================================');
+  console.log(check);
+  console.log('====================================');
 })
