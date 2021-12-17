18c3d2579990d5c7ce1f3c33fd67d86ca82c855a
Johnathan <jonathanumana86@gmail.com>
2021-12-17 01:05:48 -0500

    Creating a variable btn Check so it can be easier to call btn

--- script.js
+++ script.js
@@ -4,8 +4,12 @@
 // const overlay = document.querySelector('.overlay')
 // const btnCloseModal = document.querySelector('.close-modal')
 // const btnsSHowModal = document.querySelectorAll('.show-modal')
-document.getElementById('squaredFour').addEventListener('click', function (){
-  console.log('====================================');
-  console.log(check);
-  console.log('====================================');
-})
+// document.getElementById('squaredFour').addEventListener('click', function (){
+//   console.log('====================================');
+//   console.log(check);
+//   console.log('====================================');
+// })
+
+const btnCheck = document.querySelector('.Check-BT')
+
+
