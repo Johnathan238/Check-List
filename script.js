4f448ff5352d9e4be94a7d692ca8d0cc086455d9
Johnathan <jonathanumana86@gmail.com>
2021-12-19 17:58:28 -0500

    The inpout is working, When I click on it, it logs to the console input clicked

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsSHowModal = document.querySelectorAll('.show-modal')
-// document.getElementById('squaredFour').addEventListener('click', function (){
-//   console.log('====================================');
-//   console.log(check);
-//   console.log('====================================');
-// })
+document.getElementById('squaredFour').addEventListener('click', function (){
+  console.log('====================================');
+  console.log(check);
+  console.log('====================================');
+})
 
 const openModal = function () {
   modal.classList.remove('hidden')
