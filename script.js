168911786ec6ad8535f07bcf85499c722ba2fb2b
Johnathan <jonathanumana86@gmail.com>
2022-02-01 18:30:29 -0500

    ID form is working, will be using this form insteasd

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsSHowModal = document.querySelectorAll('.show-modal')
-document.getElementById('squaredFour').addEventListener('click', function (){
+
+document.getElementById('check').addEventListener('click', function (){
   console.log('====================================');
   console.log(check);
   console.log('====================================');
